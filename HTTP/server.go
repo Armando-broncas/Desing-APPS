6b7fc268c6cae286f3eb3e36ca836da368f47e7d
Armando-broncas <146381786+Armando-broncas@users.noreply.github.com>
2023-10-10 10:10:18 -0600

    Update server.go

--- HTTP/server.go
+++ HTTP/server.go
@@ -1,1 +1,25 @@
 //codigo preliminar//
+package main
+
+import (
+    "fmt"
+    "net/http"
+
+)
+
+func main() {
+    // Crear un enrutador httprouter
+    router := httprouter.New()
+
+    // Definir una ruta y su manejador
+    router.GET("/saludo/:nombre", SaludoHandler)
+
+    // Iniciar el servidor HTTP en el puerto 8080
+    fmt.Println("Servidor escuchando en el puerto 8080...")
+    http.ListenAndServe(":8080", router)
+}
+
+func SaludoHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    nombre := ps.ByName("nombre")
+    fmt.Fprintf(w, "¡Hola, %s!", nombre)
+}
